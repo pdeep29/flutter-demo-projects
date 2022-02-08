d2940a2262c98c39bc8a82ded74c79045bad859a
pdeep29 <62847212+pdeep29@users.noreply.github.com>
2022-02-08 15:17:37 +0530

    commit

---
+++ blocFromZeroToHero-master/#9 - BLoC 6.1.0/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<connectivity_plus/FLTConnectivityPlugin.h>)
+#import <connectivity_plus/FLTConnectivityPlugin.h>
+#else
+@import connectivity_plus;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTConnectivityPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTConnectivityPlugin"]];
+}
+
+@end
